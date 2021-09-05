cf32691fc2c1577f85dd501a5f6b5b903006afaa
MionWakita <parisdaisy0109@gmail.com>
2021-09-05 15:51:25 +0900

    注文詳細テーブルのSeedデータ作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(UsersClassificationsTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
+        $this->call(OrdersDetailsTableSeeder::class);
     }
 }
