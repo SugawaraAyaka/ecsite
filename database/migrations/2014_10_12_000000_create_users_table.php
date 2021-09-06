39f435bc14880159927981620ec270086e50c3ce
MionWakita <parisdaisy0109@gmail.com>
2021-09-06 21:06:04 +0900

    ユーザテーブルのMigrationファイルの編集

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,18 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('password', 64);
-            $table->string('last_name', 16);
-            $table->string('first_name', 16);
-            $table->integer('zipcode')->length(8);
-            $table->string('prefecture', 16);
-            $table->string('municipality', 16);
-            $table->string('address', 32);
-            $table->string('apartments', 32)->nullable();
-            $table->string('email', 128)->unique();
-            $table->string('phone_number', 14);
-            $table->biginteger('user_classification_id')->unsigned();
-            $table->string('company_name', 128)->nullable();
+            $table->string('password', 64)->comment('パスワード');
+            $table->string('last_name', 16)->comment('苗字');
+            $table->string('first_name', 16)->comment('名前');
+            $table->integer('zipcode')->length(8)->comment('郵便番号');
+            $table->string('prefecture', 16)->comment('都道府県');
+            $table->string('municipality', 16)->comment('市町村区');
+            $table->string('address', 32)->comment('番地');
+            $table->string('apartments', 32)->comment('マンション、部屋番号')->nullable();
+            $table->string('email', 128)->unique()->comment('メールアドレス');
+            $table->string('phone_number', 14)->comment('電話番号');
+            $table->biginteger('user_classification_id')->comment('ユーザ種別')->unsigned();
+            $table->string('company_name', 128)->comment('会社名')->nullable();
             $table->char('delete_flag')->length(1);
             $table->timestamps();
 
